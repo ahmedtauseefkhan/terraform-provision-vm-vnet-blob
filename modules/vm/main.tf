5299a914a929ca5dcd61ef321abaa9ff3ff0d4df
ahmedtauseefkhan <tauseef.khan@tkxel.io>
2024-09-20 11:48:02 +0500

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "my-public-ip"
   location            = var.location
   resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"  # Or "Static" if needed
+  allocation_method   = "Static"  # Or "Static" if needed
 }
 
 resource "azurerm_network_interface" "nic" {
