c870be6b016fbdad9cbcc988f99a2ca2abdbc6fe
ahmedtauseefkhan <tauseef.khan@tkxel.io>
2024-09-20 11:44:28 +0500

    :hammer: Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,3 +1,10 @@
+resource "azurerm_public_ip" "public_ip" {
+  name                = "my-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"  # Or "Static" if needed
+}
+
 resource "azurerm_network_interface" "nic" {
   name                = "my-nic"
   location            = var.location
@@ -7,7 +14,55 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id           = azurerm_public_ip.public_ip.id  # Associate public IP
   }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "my-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_virtual_machine" "vm" {
